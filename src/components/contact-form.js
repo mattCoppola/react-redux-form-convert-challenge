e882eb8630f8f0b6804e77b95b563adc67224d8d
Matt Coppola <matt.coppola@gmail.com>
2019-01-20 09:08:27 -0600

    redux form solution

--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -1,15 +1,48 @@
 import React from 'react';
+// import redux-form
+import {reduxForm, Field} from 'redux-form';
 
-export default function ContactForm() {
-    return (
-        <form>
-            <label htmlFor="name">Your Name</label>
-            <input type="text" id="name" name="name" />
-            <label htmlFor="email">Email address</label>
-            <input type="email" id="email" name="email" />
-            <label htmlFor="message">Message</label>
-            <textarea id="message" name="message" />
-            <button>Submit</button>
-        </form>
-    );
+export class ContactForm extends React.Component {
+    onSubmit(values) {
+      console.log(values);
+    }
+
+    render() {
+      return (
+          <form
+            onSubmit={this.props.handleSubmit(values =>
+              this.onSubmit(values)
+            )}
+          >
+              <label htmlFor="name">Your Name</label>
+              <Field type="text" id="name" name="name" component="input" />
+              <label htmlFor="email">Email address</label>
+              <Field type="email" id="email" name="email" component="input" />
+              <label htmlFor="message">Message</label>
+              <Field id="message" name="message" component="textarea"/>
+              <button type="submit">Submit</button>
+          </form>
+      );
+    }
+
 }
+
+export default reduxForm({
+  form: 'contact'
+})(ContactForm);
+
+
+//
+// export default function ContactForm() {
+//     return (
+//         <form>
+//             <label htmlFor="name">Your Name</label>
+//             <input type="text" id="name" name="name" />
+//             <label htmlFor="email">Email address</label>
+//             <input type="email" id="email" name="email" />
+//             <label htmlFor="message">Message</label>
+//             <textarea id="message" name="message" />
+//             <button>Submit</button>
+//         </form>
+//     );
+// }
